a251447b2420b2fd28af83f0a04274a22b710eb6
nigefanshu <519747074@qq.com>
2018-05-10 14:56:20 +0800

    add comment to configure parameter;delete idea config file

--- p2pool/networks/bitcoin.py
+++ p2pool/networks/bitcoin.py
@@ -9,11 +9,11 @@ from p2pool.bitcoin import networks
 # changes can be done by changing one, then the other
 
 PARENT = networks.nets['bitcoin']
-SHARE_PERIOD = 30 # seconds
-CHAIN_LENGTH = 24*60*60//10 # shares
-REAL_CHAIN_LENGTH = 24*60*60//10 # shares
-TARGET_LOOKBEHIND = 200 # shares
-SPREAD = 3 # blocks
+SHARE_PERIOD = 30 # seconds,expected time to generate a share(always overhead)
+CHAIN_LENGTH = 24*60*60//10 # shares,the number of shares p2pool keeps before discarding them
+REAL_CHAIN_LENGTH = 24*60*60//10 # shares,sets the total number of previously found shares to include in the payout when a block is found
+TARGET_LOOKBEHIND = 200 # shares,Determines the number of shares counted for difficulty regulation
+SPREAD = 3 # blocks,determines how many blocks (max) to pay if a miner finds at lease one share
 IDENTIFIER = 'fc70035c7a81bc6f'.decode('hex')
 PREFIX = '2472ef181efcd37b'.decode('hex')
 P2P_PORT = 9333
@@ -26,7 +26,7 @@ MAX_TARGET = 2**256//2**28 - 1   #scchain args
 # PERSIST = True
 PERSIST = False
 WORKER_PORT = 9332
-BOOTSTRAP_ADDRS = ''.split(' ')
+BOOTSTRAP_ADDRS = ''.split(' ')#boostrap address that will autoconnect to
 ANNOUNCE_CHANNEL = '#p2pool'
 VERSION_CHECK = lambda v: None if 100000 <= v else 'Bitcoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
 VERSION_WARNING = lambda v: None
